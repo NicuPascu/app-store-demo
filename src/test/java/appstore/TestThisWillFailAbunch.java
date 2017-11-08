1ab58ef6911b70ab0cff209dd434c04034a4917e
Nicolae Pascu <contact@nicupascu.ro>
2017-11-08 18:29:10 +1100

    fail a test

--- src/test/java/appstore/TestThisWillFailAbunch.java
+++ src/test/java/appstore/TestThisWillFailAbunch.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 public class TestThisWillFailAbunch {
     @Test
     public void aFailingTest() {
-         assertTrue("I expected this to pass!", true);
+         assertTrue("I expected this to pass!", false);
     }
 
     @Ignore
